d190fd700993999e497d04134f7e376b91bce9e9
irene1881 <ireneguo@brandeis.edu>
2024-08-04 15:16:31 +0200

    filter style

--- frontend/src/components/BondFilter.js
+++ frontend/src/components/BondFilter.js
@@ -11,39 +11,57 @@ const BondFilter = ({ filters, onFilterChange }) => {
 
   return (
     <div className="bond-filter">
-      <input
-        type="text"
-        name="id"
-        placeholder="Filter by ID"
-        value={filters.id}
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        name="name"
-        placeholder="Filter by Name"
-        value={filters.name}
-        onChange={handleChange}
-      />
-      <select name="overdue" value={filters.overdue} onChange={handleChange}>
-        <option value="">Filter by Overdue</option>
-        <option value="true">Yes</option>
-        <option value="false">No</option>
-      </select>
-      <input
-        type="date"
-        name="maturityStart"
-        placeholder="Maturity Start"
-        value={filters.maturityStart}
-        onChange={handleChange}
-      />
-      <input
-        type="date"
-        name="maturityEnd"
-        placeholder="Maturity End"
-        value={filters.maturityEnd}
-        onChange={handleChange}
-      />
+      <h3>Bond Filters</h3>
+        <div className="filter-container">
+          <div className="filter-item">
+            <label htmlFor="id">ID: </label>
+            <input
+              type="text"
+              name="id"
+              placeholder="Filter by ID"
+              value={filters.id}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="filter-item">
+            <label htmlFor="name">Name: </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Filter by Name"
+              value={filters.name}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="filter-item">
+            <label htmlFor="name">Overdue: </label>
+            <select name="overdue" value={filters.overdue} onChange={handleChange}>
+              <option value="">Filter by Overdue</option>
+              <option value="true">Yes</option>
+              <option value="false">No</option>
+            </select>
+          </div>
+          <div className="filter-item">
+            <label htmlFor="name">Maturity From: </label>
+            <input
+              type="date"
+              name="maturityStart"
+              placeholder="Maturity Start"
+              value={filters.maturityStart}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="filter-item">
+            <label htmlFor="name">Maturity To: </label>
+            <input
+              type="date"
+              name="maturityStart"
+              placeholder="Maturity End"
+              value={filters.maturityEnd}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
     </div>
   );
 };
